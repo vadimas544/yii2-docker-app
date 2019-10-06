a4aa03eb6c94b4e02a40cd57628b9371f5c27eb2
vadim <vadim123544@gmail.com>
2019-10-06 06:57:13 -0700

    Initial settings

---
+++ beesweet.ua/frontend/models/Test.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+
+class Test
+{
+    public static function getNewsList()
+    {
+        $sql = 'SELECT * FROM news';
+        return Yii::$app->db->createCommand($sql)->queryAll();
+    }
+}
---
+++ beesweet.ua/frontend/views/test/index.php
@@ -0,0 +1,5 @@
+<?php foreach ($list as $item) : ?>
+    <h1><?= $item['title'] ?></h1>
+    <p><?= $item['content'] ?></p>
+<?php endforeach; ?>
+
